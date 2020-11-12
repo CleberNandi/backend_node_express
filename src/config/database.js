6c97f48762ad28e76d6824e9c8874af3c4f86b8a
CleberNandi <cleber.nandi@gmail.com>
2020-11-12 19:37:49 -0300

    Criado conexão com banco de dados postgres atraves de sequelize. Criado tabela users atraves das migrations

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'dbaaccesskey',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        undescored: true,
+    },
+}
